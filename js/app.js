5b21078d04862cfff12287efce1ec50ff52c3a83
Patryk <patlaw96@gmail.com>
2015-02-20 21:14:13 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,61 @@
+(function(){
+
+	var app = angular.module('store', ['cart']);
+
+	app.controller('sell', ['$scope', function(scope){
+		scope.tab = 1;
+		scope.socks = socks;
+		scope.actualStars = actualStars;
+		
+		scope.setStars = function(id, value){
+			actualStars[id] = value;
+		}
+	}]);
+	
+	var actualStars = [
+		
+	]
+	
+	var reviews = [
+		{
+			stars: 4,
+			author: "Jeremy",
+			text: "I love that one, my partner says I look sexy at those."
+		},
+		{
+			stars: 0,
+			author: "Anonymous",
+			text: "send this to 5 people or you will be dead"
+		},
+		{
+			stars: 2,
+			author: "Troll",
+			text: "BLAH BLAH BlAH! IM MASZTAROWSKI BLAH!"
+		}
+	];
+
+	var socks = [
+		{
+			name: 'Red socks',
+			description: 'lovely reddish socks',
+			price: 2.05,
+			img: "red.jpg",
+			reviews: [reviews[0]]
+		},
+		{
+			name: 'Blue socks',
+			description: 'lovely bluish socks',
+			price: 3.52,
+			img: "blue.jpg",
+			reviews: [reviews[1]]
+		},
+		{
+			name: 'Colorful socks',
+			description: 'lovely colorful socks',
+			price: 5.55,
+			img: "colorful.jpg",
+			reviews: [reviews[2]]
+		}
+	];
+
+})();
